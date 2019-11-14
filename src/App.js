b20f33329881aef758a68faa5bc35e20f9426e86
Anna Szmyga <anna.szmyga@o2.pl>
2019-11-14 10:26:01 +0100

    Retreive users data from API

--- src/App.js
+++ src/App.js
@@ -4,8 +4,26 @@ import './App.scss';
 import Search from './components/Search/Search';
 import UsersList from './components/UsersList/UsersList';
 
+const API_URL = 'https://jsonplaceholder.typicode.com/users';
+
 class App extends React.Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    fetch(API_URL)
+    .then(response => response.json())
+    .then(users => {
+        this.setState({ users });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <h1>Users list</h1>
